3e4e9d6b0632dd9fe64046a76a79cf34b15d91f3
GajewyJ <90028969+GajewyJ@users.noreply.github.com>
2024-05-04 13:53:13 +0200

    feat: Add upcoming or ongoing rally recognition in upcomingRalliesBar

--- client/src/components/upcomingRalliesBar/index.tsx
+++ client/src/components/upcomingRalliesBar/index.tsx
@@ -28,25 +28,63 @@ function UpcomingRalliesBar(){
     fetchData();
   }, []);
 
-  return (
-    <Container>
-      <Row>
-        {rallies.map((rally, index) => (
-          <Col sm={4} key={index}>
-            <Card className="mb-4">
-              <Card.Body>
-                <Card.Title>{rally.name}</Card.Title>
-                <Card.Text>
-                  <strong>From:</strong> {rally.beginning.toString().substring(0, 10)}<br/>
-                  <strong>To:</strong> {rally.end.toString().substring(0, 10)}
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </Container>
-  );
+  const [ongoingRally, setOngoingRally] = useState<Rally[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<Rally[]>('http://localhost:3000/wrcrallies/ongoing');
+        setOngoingRally(response.data);
+      } catch (error) {
+        console.error('Error fetching data', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if(ongoingRally[0] === undefined){
+    return (
+      <Container>
+        <Row>
+          {rallies.map((rally, index) => (
+            <Col sm={4} key={index}>
+              <Card className="mb-4">
+                <Card.Body>
+                  <Card.Title>Upcoming: {rally.name}</Card.Title>
+                  <Card.Text>
+                    <strong>From:</strong> {rally.beginning.toString().substring(0, 10)}<br/>
+                    <strong>To:</strong> {rally.end.toString().substring(0, 10)}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    );
+  }
+  else{
+    return (
+      <Container>
+        <Row>
+          {ongoingRally.map((rally, index) => (
+            <Col sm={4} key={index}>
+              <Card className="mb-4">
+                <Card.Body>
+                  <Card.Title>Ongoing rally: {rally.name}</Card.Title>
+                  <Card.Text>
+                    <strong>From:</strong> {rally.beginning.toString().substring(0, 10)}<br/>
+                    <strong>To:</strong> {rally.end.toString().substring(0, 10)}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    );
+  }
 };
 
 export default UpcomingRalliesBar;
