3b8ccf7cc6f9c96f9bf3f3a920e5f52aaea313fc
GajewyJ <90028969+GajewyJ@users.noreply.github.com>
2024-05-05 11:06:21 +0200

    feat: Add WRC route

--- client/src/helpers/routes.tsx
+++ client/src/helpers/routes.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Home from "../pages/home"
+import WRC from "../pages/wrc"
 
 interface RouteItem{
     path: string,
@@ -12,5 +13,10 @@ export const routes: Array<RouteItem> = [
         path: "/",
         element: <Home/>,
         label: "Home"
+    },
+    {
+        path: "/wrc",
+        element: <WRC/>,
+        label: "WRC"
     },
 ]
