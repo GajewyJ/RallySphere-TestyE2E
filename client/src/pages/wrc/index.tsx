185380478fb20627538e2802abdbf4c706a27518
GajewyJ <90028969+GajewyJ@users.noreply.github.com>
2024-05-05 11:10:25 +0200

    feat: Create WRC page template

---
+++ client/src/pages/wrc/index.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import UpcomingRalliesBar from "../../components/upcomingRalliesBar";
+import "./index.scss"
+
+function WRC(){
+  let date = new Date(Date.now())
+  let year = date.getFullYear()
+  useEffect(() => {
+      document.title = 'RallySphere - WRC ' + year;
+  }, []);
+  return (
+    <>
+        <UpcomingRalliesBar/>
+    </>
+  );
+};
+
+export default WRC;
