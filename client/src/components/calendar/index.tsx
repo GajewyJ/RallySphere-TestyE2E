90b1f74e2bc034036c41c0e959d883a592300bf5
GajewyJ <90028969+GajewyJ@users.noreply.github.com>
2024-05-05 14:24:25 +0200

    feat: Create WRC calendar for current season

---
+++ client/src/components/calendar/index.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './index.scss'
+
+interface Rally {
+  id: number;
+  name: string;
+  season: string;
+  country: string;
+  beginning: Date;
+  end: Date;
+}
+
+function Calendar(){
+  const [rallies, setRallies] = useState<Rally[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<Rally[]>('http://localhost:3000/wrcrallies');
+        setRallies(response.data);
+      } catch (error) {
+        console.error('Error fetching data', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return(
+    <main>
+      {rallies.map((rally, index) => {
+        const currentYear = new Date().getFullYear();
+        if (parseInt(rally.season) === currentYear) {
+          return (
+            <a key={rally.id} href='/'>
+              <div>
+                <p>{
+                  new Date(rally.end) < new Date() 
+                    ? "COMPLETED " 
+                    : `${new Date(rally.beginning).toLocaleDateString('pl-PL')} - ${new Date(rally.end).toLocaleDateString('pl-PL')} `
+                }</p>
+                <p>{rally.name}</p>
+                <div>
+                  <p> {rally.country}</p>
+                  <img src={`public/flags/${rally.country}.png`}/>
+                </div>
+              </div>
+            </a>
+          )
+        }
+        return null;
+      })}
+    </main>
+  )
+};
+
+export default Calendar;
