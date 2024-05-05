b7d36994d4d382a2daa0bf952e10f3cd7988177d
GajewyJ <90028969+GajewyJ@users.noreply.github.com>
2024-05-05 11:09:44 +0200

    feat: Add dynamic page title

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import UpcomingRalliesBar from "../../components/upcomingRalliesBar";
 import "./index.scss"
 
 function Home(){
+  useEffect(() => {
+      document.title = 'RallySphere - Homepage';
+  }, []);
+
   return (
     <>
         <UpcomingRalliesBar/>
