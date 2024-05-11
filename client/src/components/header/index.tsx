c7528b19eb0955c055025eb16d7d6eca90294129
GajewyJ <90028969+GajewyJ@users.noreply.github.com>
2024-05-11 15:16:44 +0200

    fix: Replace a with Link in logo section in header

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import NavBar from "../navbar";
 import "./index.scss"
 
 function Header(){
     return (
         <header>
-            <a href="/"><img src="./././logos/rallysphere-high-resolution-logo-transparent.png"/></a>
+            <Link to="/"><img src="./././logos/rallysphere-high-resolution-logo-transparent.png"/></Link>
             <NavBar/>
         </header>
     );
