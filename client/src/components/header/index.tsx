7ff19563f0750fa2b91cc035b49ff5fad412651c
GajewyJ <90028969+GajewyJ@users.noreply.github.com>
2024-05-11 17:02:59 +0200

    fix: Fix logo display

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -3,9 +3,11 @@ import NavBar from "../navbar";
 import "./index.scss"
 
 function Header(){
+    var imageBasePath = window.location.protocol + "//" + window.location.host + "/";
+
     return (
         <header>
-            <Link to="/"><img src="logos/rallysphere-high-resolution-logo-transparent.png"/></Link>
+            <Link to="/"><img src={imageBasePath + "/logos/rallysphere-high-resolution-logo-transparent.png"}/></Link>
             <NavBar/>
         </header>
     );
