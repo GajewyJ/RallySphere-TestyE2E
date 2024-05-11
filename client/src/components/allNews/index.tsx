9357bf16d800702da606f16d93ff568518394dd8
GajewyJ <90028969+GajewyJ@users.noreply.github.com>
2024-05-11 15:37:27 +0200

    feat: Create allNews component

---
+++ client/src/components/allNews/index.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './index.scss'
+import Heading from '../heading';
+
+interface News {
+  id: number;
+  title: string;
+  paragraph1: string;
+  paragraph2: string;
+  paragraph3: string;
+  photo: string;
+  publicationDate: Date;
+}
+
+function AllNews(){
+  const [news, setNews] = useState<News[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<News[]>('http://localhost:3000/news');
+        setNews(response.data);
+      } catch (error) {
+        console.error('Error fetching data', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <article>
+      <Heading level={1}>WRC News</Heading>
+      <div>
+        {news.map((newsItem) => (
+          <a href={'http://localhost:3000/news/' + newsItem.id.toString()} key={newsItem.id}>
+            <img src={newsItem.photo} alt={newsItem.title}/>
+            <p>{newsItem.publicationDate.toString().substring(8, 10) + '.' + newsItem.publicationDate.toString().substring(5, 7) + '.' + newsItem.publicationDate.toString().substring(0, 4) + ' ' + newsItem.publicationDate.toString().substring(11, 13) + ':' + newsItem.publicationDate.toString().substring(14, 16)}</p>
+            <p>{newsItem.title}</p>
+          </a>
+        ))}
+      </div>
+    </article>
+  );
+};
+
+export default AllNews;
