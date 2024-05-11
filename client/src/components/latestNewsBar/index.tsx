0c7178809ad429c9d74abf79e58e7e0000604d76
GajewyJ <90028969+GajewyJ@users.noreply.github.com>
2024-05-11 16:25:33 +0200

    fix: Fix links to news by id

--- client/src/components/latestNewsBar/index.tsx
+++ client/src/components/latestNewsBar/index.tsx
@@ -34,7 +34,7 @@ function LatestNewsBar(){
       <Heading level={1}>Latest WRC News</Heading>
       <div>
         {news.map((newsItem) => (
-          <a href={'http://localhost:3000/news/' + newsItem.id.toString()} key={newsItem.id}>
+          <a href={'/wrc/news/' + newsItem.id.toString()} key={newsItem.id}>
             <img src={newsItem.photo} alt={newsItem.title}/>
             <p>{newsItem.publicationDate.toString().substring(8, 10) + '.' + newsItem.publicationDate.toString().substring(5, 7) + '.' + newsItem.publicationDate.toString().substring(0, 4) + ' ' + newsItem.publicationDate.toString().substring(11, 13) + ':' + newsItem.publicationDate.toString().substring(14, 16)}</p>
             <p>{newsItem.title}</p>
