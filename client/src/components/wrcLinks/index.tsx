154b8af9a3ea4def98729dfadbbded446323a254
GajewyJ <90028969+GajewyJ@users.noreply.github.com>
2024-05-17 19:44:40 +0200

    feat: Rebuild and redesign WRC Links component

--- client/src/components/wrcLinks/index.tsx
+++ client/src/components/wrcLinks/index.tsx
@@ -1,20 +1,48 @@
 import { Link } from "react-router-dom";
-import Heading from "../heading";
 
 import "./index.scss"
 
 function WRCLinks(){
   return (
     <div className="WRCLinksDiv1">
-    <Heading level={1}>Useful WRC Information</Heading>
       <div>
         <div>
-          <Link to="/wrc/news">WRC News</Link>
-          <Link to="/wrc/calendar">Calendar</Link>
-          <Link to="/wrc/drivers">Driver Standings</Link>
-          <Link to="/wrc/coDrivers">Co-Driver Standings</Link>
-          <Link to="/wrc/teams">Team Standings</Link>
-          <a href="https://www.rally.tv/" target="_blank">Watch</a>
+          <div>
+            <Link to="/wrc/news">
+              <img src="/links-images/fab98f94-969a-4ffe-a7b3-ea2b263503c5_170524msfourmaux1.jpg"/>
+              <p>WRC News</p>
+            </Link>
+          </div>
+          <div>
+            <Link to="/wrc/calendar">
+              <img src="/links-images/8c4708dd-02da-4a4b-a4f4-fed8729cb0e1_140524rbfafe1.jpg"/>
+              <p>Calendar</p>
+            </Link>
+          </div>
+          <div>
+            <Link to="/wrc/drivers">
+              <img src="/links-images/7d59ea8d-baf5-49c2-927b-76fa6f252a6a_150524tgr-ogier1.jpg"/>
+              <p>Driver Standings</p>
+            </Link>
+          </div>
+          <div>
+            <Link to="/wrc/coDrivers">
+              <img src="/links-images/90f716e8-f3c0-4135-bbec-4e531ecfaf33_120524atogiersun-1.jpg"/>
+              <p>Co-Driver Standings</p>
+            </Link>
+          </div>
+          <div>
+            <Link to="/wrc/teams">
+              <img src="/links-images/faf7b01c-f6ed-43cb-a761-8fbd44fa798f_160524rbmunster.jpg"/>
+              <p>Team Standings</p>
+            </Link>
+          </div>
+          <div>
+            <Link to="https://www.rally.tv/" target="_blank">
+              <img src="/links-images/93a6481d-65e3-4dbd-9a6f-c2e55009ee07_150524hmsgtmwhts.jpg"/>
+              <p>Watch</p>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
