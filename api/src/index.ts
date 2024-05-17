259f33e8871f6bc01d892a6313d10868520d60b7
GajewyJ <90028969+GajewyJ@users.noreply.github.com>
2024-05-17 18:40:10 +0200

    feat: Add WRC Drivers Router

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response , Application } from 'express';
 import ralliesRouter from './routes/wrcRalliesRouter';
 import teamsRouter from './routes/wrcTeamsRouter';
 import newsRouter from './routes/newsRouter';
+import wrcDriversRouter from './routes/wrcDriversRouter';
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
@@ -13,6 +14,7 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/wrcRallies', ralliesRouter)
 app.use('/wrcTeams', teamsRouter);
 app.use('/news', newsRouter);
+app.use('/wrcDrivers', wrcDriversRouter);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
---
+++ api/src/routes/wrcDriversRouter.ts
@@ -0,0 +1,88 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+const wrcDriversRouter = express.Router();
+
+wrcDriversRouter.use(express.json());
+wrcDriversRouter.use(express.urlencoded({ extended: true }));
+
+wrcDriversRouter.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+const ERROR_404 = {error: '404 Not Found'};
+
+// Get all drivers
+wrcDriversRouter.get('/', async (req, res) => {
+    try {
+        const drivers = await prisma.wrc_drivers.findMany({orderBy: {points: 'desc'}});
+        if(drivers != null) {
+            res.json(drivers);
+        }
+        else{
+            res.status(404).json(ERROR_404);
+        }
+    } 
+    catch (error) {
+        res.status(500).json({ error: 'Error fetching data from the drivers table' });
+    }
+});
+
+// Get driver by ID
+wrcDriversRouter.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const driver = await prisma.wrc_drivers.findUnique({where: {id: parseInt(id)}})
+        if(driver != null) {
+            res.json(driver);
+        }
+        else{
+            res.status(404).json(ERROR_404);
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching data from the drivers table' });
+    }
+});
+
+// Create a new driver
+wrcDriversRouter.post('/', async (req, res) => {
+    const { firstName, lastName, country, team, points } = req.body;
+    try {
+        const newDriver = await prisma.wrc_drivers.create({
+        data: { firstName, lastName, country, team, points },
+        });
+        res.json(newDriver);
+    } catch (error) {
+        res.status(500).json({ error: 'Error adding record to the drivers table' });
+    }
+});
+
+// Update a driver
+wrcDriversRouter.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { firstName, lastName, country, team, points } = req.body;
+    try {
+        const updatedDriver = await prisma.wrc_drivers.update({
+        where: { id: parseInt(id) },
+        data: { firstName, lastName, country, team, points },
+        });
+        res.json(updatedDriver);
+    } catch (error) {
+        res.status(500).json({ error: 'Error updating driver' });
+    }
+});
+
+// Delete a driver
+wrcDriversRouter.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        await prisma.wrc_drivers.delete({ where: { id: parseInt(id) } });
+        res.json({ message: 'Data deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ error: 'Error deleting data' });
+    }
+});
+
+export default wrcDriversRouter;
