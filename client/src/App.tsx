d9f5a98e1e3acc5ed4bcb274590106bcba26a295
GajewyJ <90028969+GajewyJ@users.noreply.github.com>
2024-05-24 18:30:59 +0200

    feat: Add footer to App.tsx and make it display correctly

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,21 +3,25 @@ import Header from './components/header'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { routes } from './helpers/routes'
 import UpcomingRalliesBar from './components/upcomingRalliesBar'
+import Footer from './components/footer'
 
 function App() {
   return (
     <Router>
-      <Header/>
-      <UpcomingRalliesBar/>
-      <Routes>
-        {routes.map((route) => (
-            <Route
-            key={route.path}
-            path={route.path}
-            element={route.element}
-            />
-          ))}
-      </Routes>
+      <div className='content'>
+        <Header/>
+        <UpcomingRalliesBar/>
+        <Routes>
+          {routes.map((route) => (
+              <Route
+              key={route.path}
+              path={route.path}
+              element={route.element}
+              />
+            ))}
+        </Routes>
+      </div>
+      <Footer/>
     </Router>
   )
 }
