8cf40b68bda9ab376fb3616e463ac0aa22ab5fde
GajewyJ <90028969+GajewyJ@users.noreply.github.com>
2024-05-26 19:48:46 +0200

    feat: Add admin panel template with working signing in

---
+++ client/src/pages/adminPanel/index.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import "./index.scss";
+import AdminLogin from "../../components/adminLogin";
+
+// Define a type for the admin details
+type AdminDetails = {
+  id: Number;
+  username: string;
+  password: string;
+};
+
+function AdminPanel() {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [adminDetails, setAdminDetails] = useState<AdminDetails | null>(null);
+
+  useEffect(() => {
+    document.title = 'RallySphere - Admin Panel';
+    const loggedInStatus = localStorage.getItem('isLoggedIn');
+    const storedAdminDetails = localStorage.getItem('adminDetails');
+    if (storedAdminDetails) {
+      setIsLoggedIn(true);
+      setAdminDetails(JSON.parse(storedAdminDetails));
+    } else {
+      setIsLoggedIn(loggedInStatus === 'true');
+    }
+  }, []);
+
+  const handleLoginSuccess = (admin: AdminDetails) => {
+    localStorage.setItem('adminDetails', JSON.stringify(admin));
+    setAdminDetails(admin);
+    setIsLoggedIn(true);
+  };
+
+  const handleLoginError = (error: string) => {
+    alert(error);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('adminDetails');
+    setAdminDetails(null);
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <>
+      {isLoggedIn ? (
+        <div className="login-status">
+          Logged in as {adminDetails?.username}
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <AdminLogin
+          onLoginSuccess={handleLoginSuccess}
+          onLoginError={handleLoginError}
+        />
+      )}
+    </>
+  );
+}
+
+export default AdminPanel;
