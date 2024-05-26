bc5b55f3351ebe6eb6f520dd7126721dbd93eca0
GajewyJ <90028969+GajewyJ@users.noreply.github.com>
2024-05-26 18:24:39 +0200

    feat: Add footer content and correct display

--- client/src/components/footer/index.tsx
+++ client/src/components/footer/index.tsx
@@ -1,8 +1,13 @@
+import Heading from "../heading";
 import "./index.scss"
 
 function Footer(){
     return (
         <footer>
+            <div>
+                <Heading level={3}>©2024 Jakub Gajewy</Heading>
+                <p>*Some data may be inaccurate or incomplete</p>
+            </div>
         </footer>
     );
 };
