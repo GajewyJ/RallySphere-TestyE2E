ff3b52720fd161cb0d363f41c1ccfd199df2cf3f
GajewyJ <90028969+GajewyJ@users.noreply.github.com>
2024-05-29 22:21:36 +0200

    feat: Add tests to adminTeams component

---
+++ client/src/components/adminTeams/adminTeams.test.tsx
@@ -0,0 +1,83 @@
+import '@testing-library/jest-dom';
+import { render, screen, waitFor } from '@testing-library/react';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import AdminTeams from './index';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+describe('AdminTeams component', () => {
+    let mock: MockAdapter;
+
+    beforeEach(() => {
+        mock = new MockAdapter(axios);
+    });
+
+    afterEach(() => {
+        mock.restore();
+    });
+
+    it('renders correctly with teams', async () => {
+        const data = [
+            {
+                id: 1,
+                name: 'Team 1',
+                basedIn: 'Country 1',
+                establishment: 2000,
+                principal: 'Principal 1',
+                category: 'Category 1',
+                brand: 'Brand 1',
+                points: 100
+            },
+            {
+                id: 2,
+                name: 'Team 2',
+                basedIn: 'Country 2',
+                establishment: 2000,
+                principal: 'Principal 2',
+                category: 'Category 2',
+                brand: 'Brand 2',
+                points: 200
+            }
+        ];
+    
+        mock.onGet('http://localhost:3000/wrcTeams').reply(200, data);
+    
+        render(
+            <Router>
+                <AdminTeams />
+            </Router>
+        );
+    
+        await waitFor(() => screen.getByText('Team 1'));
+        await waitFor(() => screen.getByText('Team 2'));
+    
+        const teamNames = screen.getAllByText(/Team \d/);
+        expect(teamNames).toHaveLength(2);
+    });
+
+    it('checks if add button exists', async () => {
+        mock.onGet('http://localhost:3000/wrcTeams').reply(200, []);
+        mock.onPost('http://localhost:3000/wrcTeams').reply(200, {
+            id: 3,
+            name: 'Team 3',
+            basedIn: 'Country 3',
+            establishment: 2000,
+            principal: 'Principal 3',
+            category: 'Category 3',
+            brand: 'Brand 3',
+            points: 300
+        });
+    
+        render(
+            <Router>
+                <AdminTeams />
+            </Router>
+        );
+    
+        await waitFor(() => expect(screen.queryByText('Loading...')).not.toBeInTheDocument());
+    
+        const addButton = screen.getByRole('button', { name: /Add a new Team/i });
+        expect(addButton).toBeVisible();
+    });
+    
+});
