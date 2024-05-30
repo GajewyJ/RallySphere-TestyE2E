ca9582cb85c600d0eaae47e58ff501c896c7bcf7
GajewyJ <90028969+GajewyJ@users.noreply.github.com>
2024-05-30 15:46:40 +0200

    feat: Add encyclopediaLink component to home page

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import "./index.scss"
 import LatestNewsBar from "../../components/latestNewsBar";
 import WRCLinksBar from "../../components/wrcLinksBar";
+import EncyclopediaLink from "../../components/encyclopediaLink";
 
 function Home(){
   useEffect(() => {
@@ -12,6 +13,7 @@ function Home(){
     <>
         <LatestNewsBar/>
         <WRCLinksBar/>
+        <EncyclopediaLink/>
     </>
   );
 };
